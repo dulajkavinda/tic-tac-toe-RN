1c9a68f1e28fa1c3891204e39a092c738b634dfc
dulajkavinda <dulajkaavinda@gmail.com>
2019-08-16 17:05:37 +0530

    Added winning logic

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import { Entypo } from "@expo/vector-icons";
+import { Button } from "native-base";
+
+let itemArray = new Array(9).fill("empty");
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -9,6 +14,105 @@ export default class App extends React.Component {
       winMessage: ""
     };
   }
+
+  drawItem = itemNumber => {
+    if (itemArray[itemNumber] === "empty") {
+      itemArray[itemNumber] = this.state.isCross;
+
+      this.setState({ isCross: !itemArray[itemNumber] }, () => {});
+    }
+    this.winGame();
+  };
+
+  chooseItemIcon = itemNumber => {
+    if (itemArray[itemNumber] !== "empty") {
+      return itemArray[itemNumber] ? "cross" : "circle";
+    }
+    return "pencil";
+  };
+
+  chooseItemColor = itemNumber => {
+    if (itemArray[itemNumber] !== "empty") {
+      return itemArray[itemNumber] ? "red" : "green";
+    }
+    return "black";
+  };
+
+  resetGame = () => {
+    itemArray.fill("empty");
+    this.setState({ winMessage: "" });
+
+    // froce update to the component
+    this.forceUpdate();
+  };
+
+  winGame = () => {
+    if (
+      itemArray[0] !== "empty" &&
+      itemArray[0] == itemArray[1] &&
+      itemArray[1] == itemArray[2]
+    ) {
+      this.setState({
+        winMessage: (itemArray[0] ? "Cross" : "Circle").concat(" win")
+      });
+    } else if (
+      itemArray[3] !== "empty" &&
+      itemArray[3] == itemArray[4] &&
+      itemArray[4] == itemArray[5]
+    ) {
+      this.setState({
+        winMessage: (itemArray[0] ? "Cross" : "Circle").concat(" win")
+      });
+    } else if (
+      itemArray[6] !== "empty" &&
+      itemArray[6] == itemArray[7] &&
+      itemArray[7] == itemArray[8]
+    ) {
+      this.setState({
+        winMessage: (itemArray[6] ? "Cross" : "Circle").concat(" win")
+      });
+    } else if (
+      itemArray[0] !== "empty" &&
+      itemArray[0] == itemArray[3] &&
+      itemArray[3] == itemArray[6]
+    ) {
+      this.setState({
+        winMessage: (itemArray[0] ? "Cross" : "Circle").concat(" win")
+      });
+    } else if (
+      itemArray[1] !== "empty" &&
+      itemArray[1] == itemArray[4] &&
+      itemArray[4] == itemArray[7]
+    ) {
+      this.setState({
+        winMessage: (itemArray[1] ? "Cross" : "Circle").concat(" win")
+      });
+    } else if (
+      itemArray[2] !== "empty" &&
+      itemArray[2] == itemArray[5] &&
+      itemArray[5] == itemArray[8]
+    ) {
+      this.setState({
+        winMessage: (itemArray[2] ? "Cross" : "Circle").concat(" win")
+      });
+    } else if (
+      itemArray[0] !== "empty" &&
+      itemArray[0] == itemArray[4] &&
+      itemArray[4] == itemArray[8]
+    ) {
+      this.setState({
+        winMessage: (itemArray[0] ? "Cross" : "Circle").concat(" win")
+      });
+    } else if (
+      itemArray[2] !== "empty" &&
+      itemArray[2] == itemArray[4] &&
+      itemArray[4] == itemArray[6]
+    ) {
+      this.setState({
+        winMessage: (itemArray[2] ? "Cross" : "Circle").concat(" win")
+      });
+    }
+  };
 
   render() {
     return (
